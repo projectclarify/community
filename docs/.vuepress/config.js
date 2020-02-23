92a3b5dbdc090ac15d1c9a17cae3f3f438f9ffa4
Christopher Beitel <cwbeitel@users.noreply.github.com>
2020-02-23 13:54:35 -0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,7 +37,7 @@ const sidebar = [
         title: 'Archive',
         collapsable: true,
         children: [
-          '/project/archive/'
+          '/projects/archive/'
         ]
       }
     ],
